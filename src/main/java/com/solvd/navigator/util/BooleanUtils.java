576f3d2813d34019888dcf7a34c4275fe58690ca
dctii <danactolman@gmail.com>
2024-01-19 11:55:58 -0800

    add abstract methods for pre-existing utils; add checker for Double.NaN values

--- src/main/java/com/solvd/navigator/util/BooleanUtils.java
+++ src/main/java/com/solvd/navigator/util/BooleanUtils.java
@@ -7,17 +7,21 @@ import org.apache.commons.lang3.StringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.ZonedDateTime;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.util.function.Predicate;
 
 public class BooleanUtils {
     private static final Logger LOGGER = LogManager.getLogger(ClassConstants.BOOLEAN_UTILS);
 
+    /*
+        Abstract checkers
+    */
     public static boolean isEmptyOrNullArray(Object[] array) {
         return array == null
                 || array.length == 0
@@ -37,6 +41,30 @@ public class BooleanUtils {
         return !isEmptyOrNullMap(map);
     }
 
+    public static boolean isEmptyOrNullCollection(Collection<?> collection) {
+        return collection == null || collection.isEmpty();
+    }
+
+    public static boolean isNotEmptyOrNullCollection(Collection<?> collection) {
+        return isEmptyOrNullCollection(collection);
+    }
+
+    public static <T> boolean anyMatchInArray(T[] array, Predicate<T> predicate) {
+        return Arrays.stream(array).anyMatch(predicate);
+    }
+
+    public static <T> boolean allMatchInArray(T[] array, Predicate<T> predicate) {
+        return Arrays.stream(array).allMatch(predicate);
+    }
+
+    public static <T> boolean areEqual(T object1, T object2) {
+        return Objects.equals(object1, object2);
+    }
+
+    public static <K, V> boolean containsKeyWithPredicate(Map<K, V> map, K key) {
+        return map != null && map.containsKey(key);
+    }
+
     public static boolean isBlankString(String string) {
         return StringUtils.isBlank(string);
     }
@@ -46,41 +74,27 @@ public class BooleanUtils {
     }
 
     public static boolean areAnyStringsBlank(String... strings) {
-        return Arrays.stream(strings).anyMatch(BooleanUtils::isBlankString);
+        return anyMatchInArray(strings, BooleanUtils::isBlankString);
     }
 
+    /*
+        Specific checkers
+    */
 
     public static boolean isSameDate(LocalDate comparans, LocalDate comparandum) {
-        return comparans.isEqual(comparandum);
+        return areEqual(comparans, comparandum);
     }
 
     public static boolean isSameDate(ZonedDateTime comparans, ZonedDateTime comparandum) {
-        return comparans.isEqual(comparandum);
+        return areEqual(comparans, comparandum);
     }
 
-    public static boolean isWeekend(DayOfWeek dayOfWeek) {
-        final DayOfWeek[] weekends = {
-                DayOfWeek.SATURDAY,
-                DayOfWeek.SUNDAY
-        };
-
-        return Arrays
-                .asList(weekends)
-                .contains(dayOfWeek);
-    }
-
-    public static boolean isValidEmail(String email) {
-        int atSignPosition = email.indexOf(StringConstants.AT_SIGN);
-        int dotPosition = email.lastIndexOf(StringConstants.FULL_STOP);
-
-        return atSignPosition > 0
-                && dotPosition > atSignPosition + 1
-                && dotPosition < email.length() - 1;
-    }
 
     public static boolean isValidGraphType(GraphType graphType) {
-        return Arrays.stream(GraphType.values())
-                .anyMatch(validGraphType -> validGraphType == graphType);
+        return anyMatchInArray(
+                GraphType.values(),
+                validGraphType -> validGraphType == graphType
+        );
     }
 
     public static boolean isInvalidVertex(Vertex vertex) {
@@ -88,31 +102,42 @@ public class BooleanUtils {
     }
 
     public static boolean areInvalidVertices(Vertex... vertices) {
-        return Arrays.stream(vertices)
-                .anyMatch(BooleanUtils::isInvalidVertex);
+        return anyMatchInArray(vertices, BooleanUtils::isInvalidVertex);
     }
 
     public static boolean vertexExists(String vertexId, Map<String, Vertex> vertices) {
-        return vertices.containsKey(vertexId);
+        return containsKeyWithPredicate(vertices, vertexId);
     }
 
     public static boolean doAnyVerticesExist(Map<String, Vertex> vertices, String... vertexIds) {
-        return Arrays.stream(vertexIds).anyMatch(vertices::containsKey);
+        return anyMatchInArray(vertexIds, vertices::containsKey);
     }
 
     public static boolean edgeExists(String vertexId1, String vertexId2, Map<String, List<Edge>> adjacencyList) {
-        List<Edge> edges = adjacencyList.get(vertexId1);
         return
-                edges != null
-                        && edges.stream()
-                        .anyMatch(edge -> edge.getVertex2().getVertexId().equals(vertexId2));
+                containsKeyWithPredicate(adjacencyList, vertexId1)
+                        && adjacencyList
+                        .get(vertexId1)
+                        .stream()
+                        .anyMatch(edge -> {
+                                    String currEdgeVertexId2 = edge.getVertex2().getVertexId();
+                                    return areEqual(currEdgeVertexId2, vertexId2);
+                                }
+                        );
     }
 
     public static boolean areSameCoordinates(Vertex vertex, Vertex existingVertex) {
-        return
-                existingVertex.getCoordinates() != null
-                        && existingVertex.getCoordinates().equals(vertex.getCoordinates());
+        return areEqual(existingVertex.getPoint(), vertex.getPoint());
     }
+
+    public static boolean isDoubleNaN(double value) {
+        return Double.isNaN(value);
+    }
+
+    public static boolean areDoublesNan(Double... values) {
+        return allMatchInArray(values, BooleanUtils::isDoubleNaN);
+    }
+
 
 
     private BooleanUtils() {
