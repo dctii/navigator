8de064b5a344f1914061f747e1f828f58d45247a
dctii <danactolman@gmail.com>
2024-01-18 20:45:56 -0800

    add boolean checker methods for Graph

--- src/main/java/com/solvd/navigator/util/BooleanUtils.java
+++ src/main/java/com/solvd/navigator/util/BooleanUtils.java
@@ -1,5 +1,8 @@
 package com.solvd.navigator.util;
 
+import com.solvd.navigator.math.Edge;
+import com.solvd.navigator.math.GraphType;
+import com.solvd.navigator.math.Vertex;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -8,12 +11,13 @@ import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.ZonedDateTime;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 
 public class BooleanUtils {
     private static final Logger LOGGER = LogManager.getLogger(ClassConstants.BOOLEAN_UTILS);
 
-    // check if array is empty or null, or if an array full of null items
     public static boolean isEmptyOrNullArray(Object[] array) {
         return array == null
                 || array.length == 0
@@ -24,12 +28,25 @@ public class BooleanUtils {
         return !isEmptyOrNullArray(array);
     }
 
-    public static boolean isBlankOrEmptyString(String string) {
-        return StringUtils.isBlank(string) || StringUtils.isEmpty(string);
+    public static boolean isEmptyOrNullMap(Map<?, ?> map) {
+        return map == null
+                || map.isEmpty();
     }
 
-    public static boolean isNotBlankOrEmptyString(String string) {
-        return !isBlankOrEmptyString(string);
+    public static boolean isNotEmptyOrNullMap(Map<?, ?> map) {
+        return !isEmptyOrNullMap(map);
+    }
+
+    public static boolean isBlankString(String string) {
+        return StringUtils.isBlank(string);
+    }
+
+    public static boolean isNotBlankString(String string) {
+        return !isBlankString(string);
+    }
+
+    public static boolean areAnyStringsBlank(String... strings) {
+        return Arrays.stream(strings).anyMatch(BooleanUtils::isBlankString);
     }
 
 
@@ -59,6 +76,42 @@ public class BooleanUtils {
         return atSignPosition > 0
                 && dotPosition > atSignPosition + 1
                 && dotPosition < email.length() - 1;
+    }
+
+    public static boolean isValidGraphType(GraphType graphType) {
+        return Arrays.stream(GraphType.values())
+                .anyMatch(validGraphType -> validGraphType == graphType);
+    }
+
+    public static boolean isInvalidVertex(Vertex vertex) {
+        return vertex == null || isBlankString(vertex.getVertexId());
+    }
+
+    public static boolean areInvalidVertices(Vertex... vertices) {
+        return Arrays.stream(vertices)
+                .anyMatch(BooleanUtils::isInvalidVertex);
+    }
+
+    public static boolean vertexExists(String vertexId, Map<String, Vertex> vertices) {
+        return vertices.containsKey(vertexId);
+    }
+
+    public static boolean doAnyVerticesExist(Map<String, Vertex> vertices, String... vertexIds) {
+        return Arrays.stream(vertexIds).anyMatch(vertices::containsKey);
+    }
+
+    public static boolean edgeExists(String vertexId1, String vertexId2, Map<String, List<Edge>> adjacencyList) {
+        List<Edge> edges = adjacencyList.get(vertexId1);
+        return
+                edges != null
+                        && edges.stream()
+                        .anyMatch(edge -> edge.getVertex2().getVertexId().equals(vertexId2));
+    }
+
+    public static boolean areSameCoordinates(Vertex vertex, Vertex existingVertex) {
+        return
+                existingVertex.getCoordinates() != null
+                        && existingVertex.getCoordinates().equals(vertex.getCoordinates());
     }
 
 
