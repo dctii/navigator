6709f07de59fb5d826ee1bb4b267964bbeab67ce
dctii <danactolman@gmail.com>
2024-01-22 22:15:56 -0800

    add new exceptions

---
+++ src/main/java/com/solvd/navigator/exception/InvalidArrayException.java
@@ -0,0 +1,7 @@
+package com.solvd.navigator.exception;
+
+public class InvalidArrayException extends RuntimeException {
+    public InvalidArrayException(String message) {
+        super(message);
+    }
+}
