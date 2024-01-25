39e44055baa5216d430481e9038779cc7b1e2ea6
Irvin Campos <irvincampos76@gmail.com>
2024-01-25 11:45:29 -0800

    more DAO methods for getting all lists

--- src/main/java/com/solvd/navigator/bin/Driver.java
+++ src/main/java/com/solvd/navigator/bin/Driver.java
@@ -1,20 +1,35 @@
 package com.solvd.navigator.bin;
 
+import com.solvd.navigator.math.RoutePlan;
+import com.solvd.navigator.util.BooleanUtils;
+import com.solvd.navigator.util.ClassConstants;
+import com.solvd.navigator.util.StringFormatters;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Driver {
+    private static final Logger LOGGER = LogManager.getLogger(ClassConstants.DRIVER);
+
     private int driverId;
     private int employeeId;
     private int vehicleId;
+    private List<RoutePlan> routePlans;
 
 
     public Driver() {
-
+        this.routePlans = new ArrayList<>();
     }
 
     private Driver(Builder builder) {
         this.driverId = builder.driverId;
         this.employeeId = builder.employeeId;
         this.vehicleId = builder.vehicleId;
+        this.routePlans = new ArrayList<>();
     }
+
 
     public int getDriverId() {
         return driverId;
@@ -66,5 +81,47 @@ public class Driver {
         public Driver build() {
             return new Driver(this);
         }
+    }
+
+    public List<RoutePlan> getRoutePlans() {
+        return routePlans;
+    }
+
+    public void setRoutePlans(List<RoutePlan> routePlans) {
+        if (BooleanUtils.isEmptyOrNullCollection(routePlans)) {
+            LOGGER.error("Route plans cannot be empty or null to set");
+            throw new IllegalStateException("Route plans cannot be empty or null to set");
+        }
+        this.routePlans = routePlans;
+    }
+
+    public void addRoutePlanToList(RoutePlan newRoutePlan) {
+        if (routePlans == null) {
+            LOGGER.error("Route plan cannot be null");
+            throw new IllegalStateException("Route plan is empty");
+        }
+        routePlans.add(newRoutePlan);
+    }
+
+    public void removeRoutePlanFromList(RoutePlan newRoutePlan) {
+        if (routePlans == null) {
+            LOGGER.error("Route plan cannot be null for removal");
+            throw new IllegalStateException("Route plan cannot be null for removal");
+        }
+        routePlans.remove(newRoutePlan);
+    }
+
+    @Override
+    public String toString() {
+        Class<?> currClass = Driver.class;
+        String[] fieldNames = {
+                "driverId",
+                "employeeId",
+                "vehicleId",
+                "routePlans"
+        };
+
+        String fieldsString = StringFormatters.buildFieldsString(this, fieldNames);
+        return StringFormatters.buildToString(currClass, fieldNames, fieldsString);
     }
 }
