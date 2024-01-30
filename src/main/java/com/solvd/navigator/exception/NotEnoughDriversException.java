04b7202895517fb5720307edaf70b914e8a5b9af
dctii <danactolman@gmail.com>
2024-01-29 23:07:39 -0800

    add new exception

--- src/main/java/com/solvd/navigator/exception/NotEnoughDriversException.java
+++ src/main/java/com/solvd/navigator/exception/NotEnoughDriversException.java
@@ -1,6 +1,7 @@
 package com.solvd.navigator.exception;
-public class TooManyDriversRequestedException {
-    public static void main(String[] args) {
-        
+
+public class NotEnoughDriversException extends RuntimeException {
+    public NotEnoughDriversException(String message) {
+        super(message);
     }
 }
