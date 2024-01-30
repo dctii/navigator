1daab934c7c8841133ce1879bee9f31615a5a957
dctii <danactolman@gmail.com>
2024-01-30 06:37:08 -0800

    comment out service layer files that have unresolved parts to allow compiling

--- src/main/java/com/solvd/navigator/service/RouteService.java
+++ src/main/java/com/solvd/navigator/service/RouteService.java
@@ -1,11 +1,11 @@
-package com.solvd.navigator.service;
-
-import java.util.List;
-
-public interface RouteService {
-
-    List<RoutePlan> getRouteHistory();
-
-    DriverStatistics getDriverStatistics(int driverId);
-
-}
+//package com.solvd.navigator.service;
+//
+//import java.util.List;
+//
+//public interface RouteService {
+//
+//    List<RoutePlan> getRouteHistory();
+//
+//    DriverStatistics getDriverStatistics(int driverId);
+//
+//}
--- src/main/java/com/solvd/navigator/service/impl/LocationServiceImpl.java
+++ src/main/java/com/solvd/navigator/service/impl/LocationServiceImpl.java
@@ -1,43 +1,43 @@
-package com.solvd.navigator.service.impl;
-
-import com.solvd.navigator.bin.Location;
-import com.solvd.navigator.service.LocationService;
-
-import java.util.List;
-
-public class LocationServiceImpl implements LocationService {
-    private final Location location;
-
-    public LocationServiceImpl(Location location) {
-        this.location = location;
-    }
-
-    @Override
-    public Location getLocationById(int locationId) {
-       return location.findById(locationId);
-    }
-
-    @Override
-    public double calculateDistance(Location source, Location destination) {
-
-        //  calculate distance between two locations
-
-        double deltaX = source.getCoordinateX() - destination.getCoordinateX();
-        double deltaY = source.getCoordinateY() - destination.getCoordinateY();
-        return Math.sqrt(deltaX * deltaX + deltaY * deltaY);
-    }
-
-    @Override
-    public List<Location> getLocationsOnRoute(String routeId) {
-        // logic to retrieve locations on a specific route identified by routeId
-
-      return location.getLocationsOnRoute(routeId);
-   }
-
-    @Override
-    public Location getCurrentUserLocation() {
-
-         return location.getCurrentUserLocation();
-    }
-    }
-
+//package com.solvd.navigator.service.impl;
+//
+//import com.solvd.navigator.bin.Location;
+//import com.solvd.navigator.service.LocationService;
+//
+//import java.util.List;
+//
+//public class LocationServiceImpl implements LocationService {
+//    private final Location location;
+//
+//    public LocationServiceImpl(Location location) {
+//        this.location = location;
+//    }
+//
+//    @Override
+//    public Location getLocationById(int locationId) {
+//       return location.findById(locationId);
+//    }
+//
+//    @Override
+//    public double calculateDistance(Location source, Location destination) {
+//
+//        //  calculate distance between two locations
+//
+//        double deltaX = source.getCoordinateX() - destination.getCoordinateX();
+//        double deltaY = source.getCoordinateY() - destination.getCoordinateY();
+//        return Math.sqrt(deltaX * deltaX + deltaY * deltaY);
+//    }
+//
+//    @Override
+//    public List<Location> getLocationsOnRoute(String routeId) {
+//        // logic to retrieve locations on a specific route identified by routeId
+//
+//      return location.getLocationsOnRoute(routeId);
+//   }
+//
+//    @Override
+//    public Location getCurrentUserLocation() {
+//
+//         return location.getCurrentUserLocation();
+//    }
+//    }
+//
